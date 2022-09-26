22423f0ab0aa8a36d80288d9dc023df365abe30f
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2022-09-26 09:15:38 +0200

    lint: fix font loading

--- components/MetaTags/Meta.tsx
+++ components/MetaTags/Meta.tsx
@@ -19,10 +19,6 @@ export function Meta() {
       />
 
       <link rel="icon" href="/favicon.ico" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@600&display=swap"
-        rel="stylesheet"
-      />
     </Head>
   );
 }
