b04c5862ccc9f279a1ec167e8d73d51db54ae656
tomfa <tomas.a.fagerbekk@gmail.com>
2022-09-26 13:48:40 +0000

    Deploying to gh-pages from @ tomfa/next-template@8b261ffe88b9f1b24e5d05887280c38b7bb5b9e7 🚀

--- _next/static/9d7dd44/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-4eb00e15e9f6990d348e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8dc77db2ec647c998e41.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
